d98987e05f55cf57149b30e2fc5746df83f0cc82
WilliamNyamu <williamnyamu08@gmail.com>
2025-11-23 16:33:15 +0300

    Implemented Listings Model

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -41,15 +41,15 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'drf_yasg',
+    'listings',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'drf_yasg',
-    'listings',
 ]
 
 MIDDLEWARE = [
